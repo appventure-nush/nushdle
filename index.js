b72d742befca51102bbdd49a5a2342043b75136a
hemisemidemipresent <hnngggrrrr@gmail.com>
2022-10-12 18:39:34 +0800

    more stuff

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-for (let i = 2; i <= 6; i++) {
-    document.getElementById(`guess${i}`).disabled = true;
-    console.log(i);
+let activeGuess = 1;
+
+for (let i = 1; i <= 6; i++) {
+    // add onkeypress event listener
+
+    // 'disable' all but the first guess
+    if (i != activeGuess) document.getElementById(`guess${i}`).disabled = true;
 }
+
+function onKeyPress(event) {}
